f79ebce3ec3c4130a2154c4b98eb71cc8188dd56
Gadi Guy <gadiguy@gmail.com>
2026-03-06 18:30:45 +0200

    Fix import paths in evm-scanner and btc-scanner

--- src/btc-scanner.ts
+++ src/btc-scanner.ts
@@ -1,6 +1,6 @@
 import { BlockDb } from './db/block-db';
-import { BitcoinNode } from './btc/bitcoin-node';
-import { BtcBlockScanner } from './btc/btc_block_scanner';
+import { BitcoinNode } from './btc-listener/bitcoin-node';
+import { BtcBlockScanner } from './btc-listener/btc-block-scanner';
 
 const scanner = new BtcBlockScanner(new BlockDb(), new BitcoinNode());
 scanner.run();
--- src/evm-scanner.ts
+++ src/evm-scanner.ts
@@ -1,7 +1,7 @@
 import { BlockDb } from './db/block-db';
-import { BlockProvider } from './block-provider';
-import { EventWriter } from './event-writer';
-import { BlockScanner } from './block-scanner';
+import { BlockProvider } from './evm-listener/block-provider';
+import { EventWriter } from './evm-listener/event-writer';
+import { BlockScanner } from './evm-listener/block-scanner';
 
 const scanner = new BlockScanner(new BlockDb(), new BlockProvider(), new EventWriter());
 scanner.run();
